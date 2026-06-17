f2b1d2b3991f14428f6e254e21c5b24e5140737c
Joao J. Muniz <joaojmm1910@gmail.com>
2026-06-16 21:36:57 -0300

    Primeira Versão do Projecto

--- dados/supabase-config.js
+++ dados/supabase-config.js
@@ -2,8 +2,8 @@
    Preencha somente URL e chave anon/publishable do NOVO projeto.
    Nunca coloque service_role ou senha do banco neste arquivo. */
 window.JC_SUPABASE_CONFIG = {
-  url: 'COLE_AQUI_A_URL_DO_NOVO_SUPABASE',
-  publishableKey: 'COLE_AQUI_A_CHAVE_ANON_PUBLICA',
+  url: 'https://iwavyzzdrhbwmjjjxehr.supabase.co',
+  publishableKey: 'sb_publishable_yF5hgp0tnH5XYKSWzcYf_Q_HcT8QHhm',
   // O login por nome de usuário é resolvido pelo Supabase.
   // Não é necessário publicar seu nome ou e-mail administrativo aqui.
   adminUsername: '',
